e9fae9288521736da07e5876da8640ae91d1ae09
swissymissy <pnghi1606@gmail.com>
2025-09-17 22:35:14 -0400

    fixing parallel command implementation

--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
+/*
+ * rush - A simplified Unix shell implementation
+ *
+ * Description:
+ * This program implements a basic shell that supports interactive and batch modes.
+ * It handles built-in commands (exit, cd, path), executes external programs,
+ * supports output redirection using '>' and parallel command execution using '&'.
+ * Executables are searched using user-defined paths, validated via access().
+ *
+ * Name: Phuong Nghi Hoang
+ * Student ID: 8765-3734
+ * Date: Sep 17th 2025
+ */
+
 #include "main.h"
 #define MAX_TOKEN 100
 
@@ -6,6 +20,7 @@ void print_error_message(){
     write(STDERR_FILENO, error_message, strlen(error_message));
     fflush(NULL);
 }
+
 void split_commands(char *command, char **command_array) {
     int i = 0;
     char *token;
@@ -16,7 +31,7 @@ void split_commands(char *command, char **command_array) {
         command_array[i] = token;
         i++; 
     }
-    command_array[i] = NULL; // making sure last value is NULL
+    command_array[i] = NULL; // making sure last value is NULL terminator
 }
 
 int main(int argc, char *argv[]){
@@ -61,15 +76,15 @@ int main(int argc, char *argv[]){
             continue;
         }
 
-        //splitting full command by "&"
         char *linecopy = lineptr;
         char *commands[MAX_TOKEN];
         char *cmd;
         int num_cmd = 0;
-        
+
+        //splitting full command by "&"
         while((cmd = strsep(&linecopy, "&")) != NULL) {
             // trim leading spaces
-            while (*cmd == ' ' || *cmd == '\t') {
+            while (*cmd == ' ' || *cmd == '\t'){
                 cmd++;
             }
             if (*cmd == '\0'){
@@ -83,7 +98,7 @@ int main(int argc, char *argv[]){
             continue; //empty line, prompt again
         }
         
-        pid_t processID[MAX_TOKEN]; // array to store the processes for parallel commands
+        pid_t processID[MAX_TOKEN]; // array to store the children processes for parallel commands
     
         // process each command in commands array 
         for (int i = 0; i < num_cmd; i++){
@@ -109,6 +124,26 @@ int main(int argc, char *argv[]){
                 }
                 continue;
             } else if (strcmp(token_array[0], "path") == 0) { //path command
+                // if no argument, clear path list
+                if(token_array[1] == NULL) {
+                    for (int i = 0; i < path_count; i++){
+                        free(search_path[i]);
+                    }
+                    path_count = 0;
+                    continue;
+                }
+                // validate if the path exists and is a directory
+                int error = 0;
+                for (int i = 1; token_array[i] != NULL; i++) {
+                    if (access(token_array[i], F_OK | X_OK) != 0){ 
+                        print_error_message(); 
+                        error = 1;
+                        break;
+                    }
+                }
+                if(error) {
+                    continue; //skip updating path list
+                }
                 //free old allocated strings
                 for (int i = 0; i < path_count; i++){
                     free(search_path[i]);
@@ -127,7 +162,12 @@ int main(int argc, char *argv[]){
             char full_path[256];
             int found = 0;
             for (int i = 0; i < path_count ; i++){
-                snprintf(full_path, sizeof(full_path), "%s%s", search_path[i], token_array[0]);
+                if (search_path[i][strlen(search_path[i]) - 1] == '/'){ // checking if the path ends in '/' and adding it if needed
+                    snprintf(full_path, sizeof(full_path), "%s%s", search_path[i], token_array[0]);
+                } else {
+                    snprintf(full_path, sizeof(full_path), "%s/%s", search_path[i], token_array[0]);
+                }
+
                 if (access(full_path, X_OK) == 0) { //checking if it is executable
                     found = 1;
                     break;
@@ -141,30 +181,31 @@ int main(int argc, char *argv[]){
             //checking for redirection mark ">"
             int rdr_idx = -1;
             char *filename = NULL;
-            int error = 0; 
+            int error = 0; //flag
             for (int i = 0; token_array[i] != NULL; i++){
                 if (strcmp(token_array[i], ">") == 0){
                     if (rdr_idx != -1) { // checking for extra ">"
                         print_error_message();
                         error = 1; // flag 
                     }
-                    if (!token_array[i+1]  || token_array[i+2]) { // detect no filename given or extra arguments after filename
+                    if (token_array[i+1] == NULL  || token_array[i+2] != NULL) { // detect no filename given or extra arguments after filename
                         print_error_message(); 
                         error = 1;
                         break;
                     }
                     rdr_idx = i; // remember index of redirection 
                     filename = token_array[i+1];
-                    token_array[rdr_idx] = NULL; // cut off > and filename from token array so execv won't pass them as arguments
+                    token_array[rdr_idx] = NULL; // cut off > so execv won't pass them as arguments
+                    token_array[rdr_idx+1] = NULL; // ensure filename doesn't get passed to execv() as argument
                     break; // only support one ">"
                 }
             }
 
-            if( error ) { // if there is error flag , skip fork() and execv(), go back to prompt
+            if( error ) { // if there is error, skip fork() and execv(), go back to prompt
                 continue;
             }
         
-            //using fork(), create a child process
+            // create child process
             int rc = fork();
             if (rc < 0) {
                 //forked failed
@@ -173,7 +214,7 @@ int main(int argc, char *argv[]){
             } else if (rc == 0) {
                 //child, run a new process
                 if (rdr_idx != -1){ // checking for redirection
-                    int file_descriptor = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+                    int file_descriptor = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU); // save the descriptor of the opened file
                     if (file_descriptor < 0){
                         print_error_message();
                         exit(1);
@@ -192,7 +233,7 @@ int main(int argc, char *argv[]){
             free(commands[i]);
         }
         
-        //wait for all children to finish
+        //wait for all children process to finish
         for (int i = 0; i < num_cmd; i++){
             waitpid(processID[i], NULL, 0);
         }
